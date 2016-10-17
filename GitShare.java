d0b735b5ae743eb5dc26c762ab52dda560b64894
DESKTOP-SA20EG6\micha <michaerel_0611@yahoo.co.jp>
2016-10-17 23:50:33 +0900

    test1

--- GitShare.java
+++ GitShare.java
@@ -1,7 +1,7 @@
-
+﻿
 public class GitShare {
 
-	public static String msg="gitで共有します";
+	public static String msg="gitで共有します testx1";
 
 	public static void main (String[] args){
 	System.out.print(msg);
